e8b0bab33618567424e63544b89b13925e45fdb7
sp1deydev <duckhanhthientran@gmail.com>
2024-12-05 15:30:09 +0700

    create Income and Expense model

---
+++ server/app/models/expense.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const expenseSchema = new mongoose.Schema({
+    userId: {
+      type: String,
+      required: true,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    value: {
+      type: Number,
+      required: true,
+    },
+    type: {
+      type: String,
+      required: true,
+    },
+    note: {
+      type: String,
+      required: true,
+    },
+    date: {
+      type: String,
+      required: true,
+    },
+  }, {timestamps: true});
+
+module.exports = mongoose.model('Expense', expenseSchema);
---
+++ server/app/models/income.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const incomeSchema = new mongoose.Schema({
+    userId: {
+      type: String,
+      required: true,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    value: {
+      type: Number,
+      required: true,
+    },
+    type: {
+      type: String,
+      required: true,
+    },
+    date: {
+      type: String,
+      required: true,
+    },
+  }, {timestamps: true});
+
+module.exports = mongoose.model('Income', incomeSchema);
